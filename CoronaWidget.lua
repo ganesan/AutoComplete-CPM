740d80b9acc165ada8806332a74da895f4ccf795
lano78 <info@creativefusion.se>
2011-07-02 13:55:44 +0200

    first draft
    this version has not been tested yet.

--- CoronaWidget.lua
+++ CoronaWidget.lua
@@ -1,9 +1,14 @@
-widget.newButton (<##> )
-widget.newEmbossedText (<##> )
-widget.newPickerWheel (<##> )
-widget.newScrollView (<##> )
-widget.newSegmentedControl (<##> )
+widget.newButton { id = "<#id#>", x = <#position#>, y = <#position#>, width = <#buttonWidth#>, height = <#buttonHeight#>, label = "<#label#>", labelColor = { <#R#>, <#G#>, <#B#> }, size = <#fontSize#>, font = "<#fontName#>", onPress = <#functionName#>, onRelease = <#functionName#>, onEvent = <#functionName#>, emboss = <#BOOL#>, offset = <#number#>, default = "<#customImage#>", over = "<#customOverImage#>"  }
+widget.newEmbossedText ("<#myText#>", <#xPos#>, <#yPos#>, "<#fontName#>", <#size#>, { <#R#>, <#G#>, <#B#> })
+widget.newPickerWheel (<##>,<##>, <##>, <##>, <##>, <##>,  <##>)
+widget.newScrollView (<##>, <##>, <##>, <##>, <##>, <##>, <##>, <##> )
+widget.newSegmentedControl (<#buttonTable#>, { x = <#xPos#>, y = <#yPos#>, labelColor = { <#R#>, <#G#>, <#B#> }, font = "<#fontName#>", size = <#fontSize#>, padding = <#number#>, emboss = <#BOOL#> } )
 widget.newSlider { "<#id#>", <#xValue#>, <#yValue#>, <#width#>, <#startValue#>, <#callBack#>  }
-widget.newTableView (<##> )
+widget.newTableView (<##>, <##>, <##>, <##>, <##>, <##>, <##>, <##>,<##> )
+widget.newToolbar ("<#title#>", { <#customWidth#>, <#customHeight#>, "<#customGradient#>", <#baseDir#>, <#xPos#>, <#yPos#>, "<#fontName#>", { <#R#>, <#G#>, <#B#> }  } )
 sync(<#dataTable#>)
-widget.newToolbar (<##> )
+label = { "<#labelText#>" }
+labelColor = { <#R#>, <#G#>, <#B#> }
+setText("<#modifiedText#>", { <#R#>, <#G#>, <#B#> } )
+buttonTable = { { id = "<#id#>", label = "<#labelText#>", onPress = <#function#>, isDown = <#BOOL#> }, { id = "<#id#>", label = "<#labelText#>", onPress = <#function#> }, { id = "<#id#>", label = "<#labelText#>", onPress = <#function#> }   }
+view
