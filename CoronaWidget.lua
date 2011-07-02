c1f787e46d15ce3075f735cb19f05c23142c32bb
lano78 <info@creativefusion.se>
2011-07-02 02:16:17 +0200

    Started widget lib.
    The widget library is still in beta, please check the API section on the Ansca website.

---
+++ CoronaWidget.lua
@@ -0,0 +1,9 @@
+widget.newButton (<##> )
+widget.newEmbossedText (<##> )
+widget.newPickerWheel (<##> )
+widget.newScrollView (<##> )
+widget.newSegmentedControl (<##> )
+widget.newSlider { "<#id#>", <#xValue#>, <#yValue#>, <#width#>, <#startValue#>, <#callBack#>  }
+widget.newTableView (<##> )
+sync(<#dataTable#>)
+widget.newToolbar (<##> )
