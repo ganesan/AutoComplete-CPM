f45bee2f1424c539e12b509c2de99e35e1ae3095
lano78 <info@creativefusion.se>
2011-07-17 16:03:26 +0200

    Often used variables
    variables I use often

---
+++ CustomVars.lua
@@ -0,0 +1,7 @@
+centerX = display.contentWidth/2
+centerY = display.contentHeight/2
+_W = display.contentWidth
+_H = display.contentHeight
+screenWidth = display.contentWidth
+screenHeight = display.contentHeight
+
