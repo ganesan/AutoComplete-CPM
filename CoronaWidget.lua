e2be55f65ff7a4c701763f905057badb753f920b
lano78 <info@creativefusion.se>
2011-07-17 22:17:14 +0200

    added params for tableView
    widget.newTableView added, also when using newTableView and when calling
     sync(tableData). I made a table for populating tableViews, simply type
    tableData = { } or local tableData = { } and you will get all the curren
    t params available inside the { }.
    
    Latest updates to tableView, a few n
    ew features were added. onLeftSwipe, onRightSwipe, deleteRow() and scrol
    lToTop(). Check the documentation on that in the API section on the Ansc
    a website.

--- CoronaWidget.lua
+++ CoronaWidget.lua
@@ -5,11 +5,17 @@ widget.newPickerWheel (<##>,<##>, <##>, <##>, <##>, <##>,  <##>)
 widget.newScrollView (<##>, <##>, <##>, <##>, <##>, <##>, <##>, <##> )
 widget.newSegmentedControl (<#buttonTable#>, { x = <#xPos#>, y = <#yPos#>, labelColor = { <#R#>, <#G#>, <#B#> }, font = "<#fontName#>", size = <#fontSize#>, padding = <#number#>, emboss = <#BOOL#> } )
 widget.newSlider { "<#id#>", <#xValue#>, <#yValue#>, <#width#>, <#startValue#>, <#callBack#>  }
-widget.newTableView (<##>, <##>, <##>, <##>, <##>, <##>, <##>, <##>,<##> )
+widget.newTableView()
+widget.newTableView { width = <#widthTableItems#>, height = <#heightTableItems#>, x = <#xPos#>, y = <#yPos#>, mask = <#stringORobject#>, background = "<#fileName#>", backgroundColor = { <#R#>, <#G#>, <#B#>, <#Alpha#> }, hideScrollbar = <#true/false#>, isInfinite = <#true/false#>, bottomLineColor = { <#R#>, <#G#>, <#B#>, <#Alpha#> }, rowHeight = <#heightInPixels#>, rowColor = { <#R#>, <#G#>, <#B#>, <#Alpha#> }, downColor = { <#R#>, <#G#>, <#B#>, <#Alpha#> }, titleFont = { "<#fontName#>",  { <#R#>, <#G#>, <#B#>, <#Alpha#> }, <#fontSize#> }, subFont = { "<#fontName#>",  { <#R#>, <#G#>, <#B#>, <#Alpha#> }, <#fontSize#>  }, categoryBackground = { "<#imageFileName#>", <#width#>, >#height#> }, baseDir = <#directory#>}
 widget.newToolbar ("<#title#>", { <#customWidth#>, <#customHeight#>, "<#customGradient#>", <#baseDir#>, <#xPos#>, <#yPos#>, "<#fontName#>", { <#R#>, <#G#>, <#B#> }  } )
-sync(<#dataTable#>)
+tableData = {  icon = { image = "<#fileName#>", width = <#pixels#>, height = <#pixels#> , base = <#directory#> , paddingTop = <#pixels#> , paddingLeft = <#pixels#> , paddingRight = <#pixels#>  }, title = "<#titleText#>", subtitle = "<#description#>", onRelease = <#function#>, onLeftSwipe = <#function#>, onRightSwipe = <#function#> }
+sync(<#tableData#>)
 label = { "<#labelText#>" }
 labelColor = { <#R#>, <#G#>, <#B#> }
 setText("<#modifiedText#>", { <#R#>, <#G#>, <#B#> } )
 buttonTable = { { id = "<#id#>", label = "<#labelText#>", onPress = <#function#>, isDown = <#BOOL#> }, { id = "<#id#>", label = "<#labelText#>", onPress = <#function#> }, { id = "<#id#>", label = "<#labelText#>", onPress = <#function#> }   }
 view
+changeTitleText(<#listItem#>, "<#newTextString#>")
+changeSubText( <#listItem#>, "<#newTextString#>" )
+deleteRow(<#listItemObject#>)
+scrollToTop()
