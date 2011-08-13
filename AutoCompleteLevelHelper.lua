9d52216ef43e4ebbf0e89193e561a94b5e622387
lano78 <info@creativefusion.se>
2011-08-13 23:16:50 +0200

    Updated List
    List should work fine, Not fully tested but it contains all LevelHelper
    calls.

--- AutoCompleteLevelHelper.lua
+++ AutoCompleteLevelHelper.lua
@@ -1,7 +1,26 @@
 LevelHelperLoader:
 Loader:
+initWithContentOfFile("<#levelFileName#>")
+initWithContentOfFileFromResourceDir("<#levelFileName#>", <#SystemDocumentsDirectory#>)
+instantiateSprites()
+instantiateSpritesInGroup(<#groupName#>)
+instantiateObjects(physics)
+instantiateObjectsInGroup(physics, <#groupName#>)
+instantiateSpritesInGroup(<#groupName#>)
+spriteWithUniqueName("<#mySpriteName#>")
+isSpriteWithUniqueNameInLevel("<#mySpriteName#>")
+jointWithUniqueName("<#jointName#>")
+isJointWithUniqueNameInLevel("<#myJoint#>")
+spritesWithTag(<#LevelHelperMySpriteTag#>)
+newObjectWithUniqueName("<#mySpriteName#>", physics)
+newSpriteWithUniqueName("mySpriteName")
+removeSpriteWithUniqueName("<#mySpriteName#>")
+removeAllSprites()
+removeJointWithUniqueName("<#mySpriteName#>")
+removeAllJoints()
+removeSpritesWithTag(<#LevelHelperMySpriteTag#>)
 LevelHelperLoader:initWithContentOfFile("<#levelFileName#>")
-LevelHelperLoader:initWithContentOfFileFromResourceDir("<#levelFileName#>", "<#SystemDocumentsDirectory#>")
+LevelHelperLoader:initWithContentOfFileFromResourceDir("<#levelFileName#>", <#SystemDocumentsDirectory#>)
 Loader:instantiateSprites()
 Loader:instantiateSpritesInGroup(<#groupName#>)
 Loader:instantiateObjects(physics)
@@ -10,11 +29,28 @@ Loader:instantiateSpritesInGroup(<#groupName#>)
 Loader:spriteWithUniqueName("<#mySpriteName#>")
 Loader:isSpriteWithUniqueNameInLevel("<#mySpriteName#>")
 Loader:jointWithUniqueName("<#jointName#>")
-loader:isJointWithUniqueNameInLevel("<#myJoint#>")
+Loader:isJointWithUniqueNameInLevel("<#myJoint#>")
 Loader:spritesWithTag(<#LevelHelperMySpriteTag#>)
 Loader:newObjectWithUniqueName("<#mySpriteName#>", physics)
+Loader:newSpriteWithUniqueName("mySpriteName")
 Loader:removeSpriteWithUniqueName("<#mySpriteName#>")
 Loader:removeAllSprites()
 Loader:removeJointWithUniqueName("<#mySpriteName#>")
 Loader:removeAllJoints()
 Loader:removeSpritesWithTag(<#LevelHelperMySpriteTag#>)
+loader:allSprites()
+coronaSprite.numberOfFrames
+coronaSprite.uniqueName
+coronaSprite.LHParallaxRatioX
+coronaSprite.LHParallaxRatioY
+coronaSprite.LHParallaxContinuous
+coronaSprite.LHParallaxDirection
+coronaSprite.LHParallaxSpeed
+coronaSprite.LHParallaxInitialX
+coronaSprite.LHParallaxInitialY
+coronaSprite.bezierPathTimeInterval
+coronaSprite.bezierPathPoints
+coronaSprite.bezierPathInc
+coronaSprite.bezierPathPreviousPoint
+coronaSprite.bezierPathIsCyclic
+coronaSprite.bezierPathRestartOtherEnd
